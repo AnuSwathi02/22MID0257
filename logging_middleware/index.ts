41f13bc341316ea7042338afc2ad5c44a669eef3
AnuSwathi02 <anuswathivr02@gmail.com>
2026-05-16 16:21:10 +0530

    add logging middleware with Log function

---
+++ logging_middleware/index.ts
@@ -0,0 +1,29 @@
+const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhbnVzd2F0aGkudnIyMDIyQHZpdHN0dWRlbnQuYWMuaW4iLCJleHAiOjE3Nzg5MjgyNTgsImlhdCI6MTc3ODkyNzM1OCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjQxMmUyYmMwLWE1ZjctNGIxNi1hNmFhLThmNGNkYTgyM2ZmMyIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImFudSBzd2F0aGkgdi5yIiwic3ViIjoiMTc3NmFhNTItMGQwYS00YWI4LWJlM2UtYWZhYTQyMTFhZWFlIn0sImVtYWlsIjoiYW51c3dhdGhpLnZyMjAyMkB2aXRzdHVkZW50LmFjLmluIiwibmFtZSI6ImFudSBzd2F0aGkgdi5yIiwicm9sbE5vIjoiMjJtaWQwMjU3IiwiYWNjZXNzQ29kZSI6IlNmRnVXZyIsImNsaWVudElEIjoiMTc3NmFhNTItMGQwYS00YWI4LWJlM2UtYWZhYTQyMTFhZWFlIiwiY2xpZW50U2VjcmV0IjoiYWdudmh6bUJ6UmpBSkdCcCJ9.h6omepSxc47KdLUy-NGPbTyKPSMiOYXxxZL52dg9oEQ"; 
+
+export async function Log(
+  stack: string,
+  level: string,
+  packageName: string,
+  message: string
+): Promise<void> {
+  try {
+    const response = await fetch("http://4.224.186.213/evaluation-service/logs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({
+        stack,
+        level,
+        package: packageName,
+        message,
+      }),
+    });
+
+    const data = await response.json();
+    console.log("Log created:", data.logID);
+  } catch (error) {
+    console.error("Logging failed:", error);
+  }
+}
